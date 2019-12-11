394a21a977480c6369a717e0e2e243e3541513e3
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2019-12-11 12:01:58 -0300

    [FIX] Exports.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// import { NativeModules } from 'react-native';
+
+// const { ReactNativeToast } = NativeModules;
+
+// export default ReactNativeToast;
+
+/**
+ * This exposes the native Toast module as a JS module. This has a
+ * function 'show' which takes the following parameters:
+ *
+ * 1. String message: A string with the text to toast
+ * 2. int duration: The duration of the toast. May be Toast.SHORT or
+ *    Toast.LONG
+ */
 import { NativeModules } from 'react-native';
-
-const { ReactNativeToast } = NativeModules;
-
-export default ReactNativeToast;
+module.exports = NativeModules.Toast;
